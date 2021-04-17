aeca0182fa1815504c41d9e2e68215782f293cbc
nidhijaincs18 <nidhi.jain_cs18@gla.ac.in>
2021-04-17 23:24:37 +0530

    added add to cart functionality with local storage

--- productCategory/index.js
+++ productCategory/index.js
@@ -23,3 +23,28 @@ for (let i = 0; i < indicator.length; i++) {
         }
     }
 }
+
+$('body').on('click', '.add-to-cart ', function() {
+    var productName = $(this).closest('.card-layout ').find('.produt-name').text();
+    var price = $(this).closest('.card-layout ').find('.price').text();
+    var productImage = $(this).closest('.card-layout ').find('.product-image a img').attr("src");
+
+
+    var existing = localStorage.getItem('cartList');
+
+    // If no existing data, create an array
+    // Otherwise, convert the localStorage string to an array
+    existing = existing ? JSON.parse(existing) : [];
+
+    value = { "productImage": productImage, "productName": productName, "price": price }
+
+    // Add new data to localStorage Array
+    existing.push(value);
+    console.log(value);
+    console.log(existing);
+
+    // Save back to localStorage
+    localStorage.setItem('cartList', JSON.stringify(existing));
+
+
+});
